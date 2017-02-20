b3e0d8ccd14371a6f84daa283e6ea1e7b2cd6346
Alan Ross <me@aross.io>
2017-02-19 23:53:13 -0800

    Formatting

--- Source/React/src/examples/examples/basic-jsx-external/example.js
+++ Source/React/src/examples/examples/basic-jsx-external/example.js
@@ -1,19 +1,17 @@
-var ExampleApplication = React.createClass({
-  render: function() {
-    var elapsed = Math.round(this.props.elapsed  / 100);
-    var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
-    var message =
-      'React has been successfully running for ' + seconds + ' seconds.';
+var ExampleApplication = React.createClass( {
+	render: function()
+	{
+		var elapsed = Math.round( this.props.elapsed / 100 );
+		var seconds = elapsed / 10 + (elapsed % 10 ? "" : ".0" );
+		var message = "React has been running for " + seconds + " seconds.";
 
-    return <p>{message}</p>;
-  }
-});
+		return <p>{message}</p>;
+	}
+} );
 
-var start = new Date().getTime();
+var start = Date.now();
 
-setInterval(function() {
-  ReactDOM.render(
-    <ExampleApplication elapsed={new Date().getTime() - start} />,
-    document.getElementById('container')
-  );
-}, 50);
+setInterval( function()
+{
+	ReactDOM.render( <ExampleApplication elapsed={Date.now() - start}/>, document.getElementById( "container" ) );
+}, 50 );
